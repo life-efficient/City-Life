b8cdadc205aab5c95c0050e7938df1fe986e2564
Harry Berg <harryaberg@gmail.com>
2022-10-17 13:32:43 -0700

    Adds show method to display random image for City

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 # %%
 import os
 from pprint import pprint
+import random
+from PIL import Image
 
 
 class City:
-    pass
+    def __init__(self, city_name):
+        self.images = os.listdir(os.path.join("images", city_name))
+        self.images = [os.path.join("images", city_name, fp)
+                       for fp in self.images]
+        self.name = city_name
+
+    def __repr__(self):
+        return f"{self.name} ({len(self.images)} images)"
+
+    def show(self):
+        img_fp = random.choice(self.images)
+        img = Image.open(img_fp)
+        img.show()
 
 
 class Cities:
@@ -12,20 +26,23 @@ class Cities:
         self.cities = self.get_cities()
 
     def get_cities(self):
-        city_name_to_image_fps = {}
-        cities = os.listdir("images")
+        city_map = {}
+        city_fps = os.listdir("images")
         # print(cities)
-        for city in cities:
-            images = os.listdir(os.path.join("images", city))
-            city_name_to_image_fps[city] = images
-            # print(f"{city} ({len(images)} images)")
-        return city_name_to_image_fps
+        for city_name in city_fps:
+            city_map[city_name] = City(city_name)
+        return city_map
 
     def __repr__(self):
-        return cities.cities
+        return str(self.cities)
+
+    # TODO define how to index this class with a city name
+
+    # TODO show a random image from a random city
 
 
 if __name__ == "__main__":
     cities = Cities()
-    print(cities)
+    pprint(cities)
+    cities.cities['Beijing, China'].show()
 # %%
