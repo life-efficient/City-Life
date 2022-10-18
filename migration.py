1b52cd60a3c4d20c59538cd814469cf4c038cddc
Harry Berg <harryaberg@gmail.com>
2022-10-18 13:12:05 -0700

    Prepares migration to remove pipe from image names

---
+++ migration.py
@@ -0,0 +1,18 @@
+import os
+
+
+def remove_non_windows_friendly_characters_from_image_names():
+    city_fps = os.listdir("images")
+    for city_name in city_fps:
+        images = os.listdir(os.path.join("images", city_name))
+        image_names = [os.path.join("images", city_name, fp)
+                       for fp in images]
+        for image_name in image_names:
+            new_name = image_name.replace('|', '')
+            print('name', image_name)
+            print('new_name', new_name)
+            # os.rename(image_name, new_name)
+
+
+if __name__ == "__main__":
+    remove_non_windows_friendly_characters_from_image_names()
