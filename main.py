b4eb073e17b76f785aa755ec661b0dd548f623e0
Harry Berg <harryaberg@gmail.com>
2022-10-19 12:40:31 -0700

    Implements getitem for cities dataset

--- main.py
+++ main.py
@@ -21,9 +21,16 @@ class City:
         img.show()
 
 
-class Cities:
+class CitiesDataset:
+    """Many examples of images from different cities"""
+
     def __init__(self):
-        self.cities = self.get_cities()
+        self.cities = self.get_cities().values()
+        self.all_imgs = []
+        for city in self.cities:
+            self.all_imgs.extend(city.images)
+        # list of all of the image exmaples
+        # self.all_imgs = [*city.images for city in self.cities]
 
     def get_cities(self):
         city_map = {}
@@ -37,12 +44,20 @@ class Cities:
         return str(self.cities)
 
     # TODO define how to index this class with a city name
+    def __getitem__(self, example_idx):
+        """Get me the image of example 10 (example_idx)"""
+        return self.all_imgs[example_idx]
 
     # TODO show a random image from a random city
 
 
 if __name__ == "__main__":
-    cities = Cities()
-    pprint(cities)
-    cities.cities['Beijing, China'].show()
+    cities = CitiesDataset()
+    # pprint(cities)
+    # cities.cities['Beijing, China'].show()
+    example100 = cities[100]
+    ex10 = cities[10]
+    print(example100)
+    print(ex10)
+    # cities['Beijing, China'].show()
 # %%
