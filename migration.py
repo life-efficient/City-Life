93e97dba1ad92ad30158cfbaf86ed604317737ba
Harry Berg <harryaberg@gmail.com>
2022-11-16 21:05:45 +0000

    Updates migration to replace images names with uuids

--- migration.py
+++ migration.py
@@ -2,7 +2,13 @@
 import os
 import re
 import string
+from uuid import uuid4
 from pathlib import Path, PureWindowsPath
+
+
+def get_extension(fp):
+    ext = fp.split('.')[-1]
+    return f".{ext}"
 
 
 def remove_non_windows_friendly_characters_from_image_names():
@@ -10,37 +16,14 @@ def remove_non_windows_friendly_characters_from_image_names():
     for city_name in city_fps:
         images = os.listdir(os.path.join("images", city_name))
 
-        new_names = [os.path.join("images", city_name, PureWindowsPath(fp))
+        new_names = [os.path.join("images", city_name, str(uuid4()) + get_extension(fp))
                      for fp in images]
         original_names = [os.path.join("images", city_name, fp)
                           for fp in images]
 
         image_names = zip(new_names, original_names)
         for new_name, old_name in image_names:
-            print(new_name)
-            print(old_name)
-            print()
-            # old_name = image_name
-
-            # filename = image_name.split('/')[-1]
-            # filename = Path(image_name)
-            # windows_friendly_path = PureWindowsPath(filename)
-
-            # pattern = re.compile('[\W_]+')
-
-            # filename = pattern.sub('', filename)
-            # if old_name != windows_friendly_path:
-            #     print('name changed')
-            #     print(old_name)
-            #     print(windows_friendly_path)
-            #     print()
-
-            # image_name = image_name.replace('|', '')
-            # image_name = image_name.replace('&', '')
-            # image_name = image_name.replace(':', '')
-            # image_name = image_name.replace('?', '')
-            # image_name = image_name.replace('>', '')
-            # os.rename(old_name, image_name)
+            os.rename(old_name, new_name)
 
 
 if __name__ == "__main__":
