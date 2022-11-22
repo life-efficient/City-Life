4066c5304b6b953d3f00f553cac62bc8a7618464
Harry Berg <harryaberg@gmail.com>
2022-11-22 23:14:06 +0000

    Adds utility to evaluate model size

---
+++ utils.py
@@ -0,0 +1,17 @@
+
+def calculate_model_size(model):
+    """
+    Input: PyTorch Model
+
+    Output: Size of model parameters in MB
+    """
+    param_size = 0
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+    buffer_size = 0
+    for buffer in model.buffers():
+        buffer_size += buffer.nelement() * buffer.element_size()
+
+    size_all_mb = (param_size + buffer_size) / 1024**2
+    print('model size: {:.3f}MB'.format(size_all_mb))
+    return size_all_mb
