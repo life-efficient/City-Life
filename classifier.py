5a0395479839dbedcf8b454db34c6b61d41f13ef
Tim Howe <rrritalin@Tims-MacBook-Air.local>
2022-12-24 15:09:22 +0000

    adds self.initialise_weights_folders() to __init__(), in classifier.py

--- classifier.py
+++ classifier.py
@@ -1,7 +1,7 @@
 from dataset import CitiesDataset
 import torch
 from torchvision.models import resnet50
-
+from torchvision.models import ResNet50_Weights
 
 # class Module:
 #     def __call__(self):
@@ -70,7 +70,7 @@ class CNN(torch.nn.Module):
 class TransferLearning(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.layers = resnet50()
+        self.layers = resnet50(weights=ResNet50_Weights)
         for param in self.layers.parameters():
             param.grad_required = False
         linear_layers = torch.nn.Sequential(
@@ -81,10 +81,19 @@ class TransferLearning(torch.nn.Module):
             torch.nn.Linear(128, 10),
         )
         self.layers.fc = linear_layers
+        self.initialise_weights_folders()
         # print(self.layers)
 
     def forward(self, x):
         return self.layers(x)
+
+    def initialise_weights_folders(self):
+        ''' method to create folder for saved weights'''
+        start_time = time.strftime('%Y-%m-%d-%H:%M:%S', time.localtime())
+        folder_name=str('TransferLearning'+ start_time)
+        if not os.path.exists('model_evaluation/' + folder_name + '/saved_weights/'):
+            os.makedirs('model_evaluation/' + folder_name + '/saved_weights/') 
+        self.weights_folder_name='model_evaluation/' + folder_name + '/saved_weights/'
 
 # model = TransferLearning()
 # optimiser = torch.optim.Adam(model.feature_extractor.parameters(), lr=0.00001)
